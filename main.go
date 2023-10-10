85c21bf83927a0a81932e0974c6fc10ab86e32a4
Yahnis allegret <yahnis.allegret@yahoo.com>
2023-10-10 17:23:44 +0900

    start wit interface

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// call types
+
+type englishBot struct{}
+type spanishBot struct{}
+
+type bot interface {
+	getGreeting() string // way to call interface
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+
+}
+
+// call with type bot
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (eb englishBot) getGreeting() string {
+	// Very custom logic for generating an english greeting
+	return "Hi There! "
+}
+
+func (sb spanishBot) getGreeting() string {
+	// Very custom logic for generating an english greeting
+	return "Hola ! "
+}
